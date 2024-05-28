268a1d270941625d4bf45a8f14bdaf1914aa2f0d
iurecastro <55067724+iurecastro@users.noreply.github.com>
2024-05-28 01:26:23 +0100

    Updates

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,24 @@
+// Importa a biblioteca React
 import React from 'react';
+
+// Importa a função createRoot da biblioteca react-dom/client
 import { createRoot } from 'react-dom/client';
+
+// Importa o componente principal da aplicação
 import App from './App';
+
+// Importa a função defineCustomElements do pacote @ionic/pwa-elements/loader
 import { defineCustomElements } from '@ionic/pwa-elements/loader';
 
+// Seleciona o elemento com o ID 'root' no documento HTML
 const container = document.getElementById('root');
+
+// Cria a raiz do React a partir do contêiner selecionado
 const root = createRoot(container!);
+
+// Define os elementos customizados do Ionic PWA
 defineCustomElements(window);
+
 root.render(
   <React.StrictMode>
     <App />
--- src/pages/Tab2.tsx
+++ src/pages/Tab2.tsx
@@ -1,18 +1,41 @@
+// Importa ícones específicos da biblioteca Ionicons
 import { camera, trash, close } from 'ionicons/icons';
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar, IonFab,
-  IonFabButton,
-  IonIcon,
-  IonGrid,
-  IonRow,
-  IonCol,
-  IonImg,
-  IonActionSheet, } from '@ionic/react';
+
+// Importa componentes do Ionic React
+import { 
+  IonContent, 
+  IonHeader, 
+  IonPage, 
+  IonTitle, 
+  IonToolbar, 
+  IonFab, 
+  IonFabButton, 
+  IonIcon, 
+  IonGrid, 
+  IonRow, 
+  IonCol, 
+  IonImg, 
+  IonActionSheet 
+} from '@ionic/react';
+
+// Importa o componente ExploreContainer
 import ExploreContainer from '../components/ExploreContainer';
+
+// Importa o arquivo de estilos CSS específico para este componente
 import './Tab2.css';
 
+// Importa o hook customizado usePhotoGallery
+import { usePhotoGallery } from '../hooks/usePhotoGallery';
 
+// Define o componente funcional Tab2
 const Tab2: React.FC = () => {
+  
+  // Usa o hook usePhotoGallery e desestrutura a função takePhoto
+  const { takePhoto } = usePhotoGallery();
+  
+  
   return (
+    // Componente principal da página
     <IonPage>
       <IonHeader>
         <IonToolbar>
